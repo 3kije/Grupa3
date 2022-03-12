1d0d3547fadad4938c6bb2d2f3dd367675fc33ba
3kije <martahyjek043@gmail.com>
2022-03-12 11:17:34 +0100

    platform assets

--- Assets/Scripts/PlatformFalling.cs
+++ Assets/Scripts/PlatformFalling.cs
@@ -7,18 +7,20 @@ public class PlatformFalling : MonoBehaviour
 
     Rigidbody2D rb;
     public float timeFall;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Equals("Player"))
         {
+            animator.SetBool("IsFalling", true);
             Invoke("PlatformFall", timeFall);
             Destroy(gameObject, 2f);
         }
