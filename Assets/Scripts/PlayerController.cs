2dfa4335412de208b912d958c9333bdd4604ceb5
3kije <martahyjek043@gmail.com>
2022-04-10 12:26:10 +0200

    ending_edit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     public float maxSpeed = 10f;
     bool facingRight = true;
-    
+
     Rigidbody rb2d;
 
     bool grounded = false;
     public Transform groundCheck;
     float groundRadius = 0.5f;
     public LayerMask whatIsGround;
-    public float jumpForce = 700f;
+    public float jumpForce = 10f;
 
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+  
+  
+
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -30,25 +39,44 @@ public class PlayerController : MonoBehaviour
             Flip();
         else if (move < 0 && facingRight)
             Flip();
+
+        animator.SetFloat("Speed", Mathf.Abs(move));
     }
+
+
 
     void Update()
     {
+       
         if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
-          
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
+            //rb2d.AddForce(new Vector2(0, jumpForce));
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
+            animator.SetBool("isJumping", true);
         }
 
+        if (GetComponent<Rigidbody2D>().velocity.y < 0 && !grounded)
+        {
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isFalling", true);
+        }
+        else/* if (rb.velocity.y >= 0)*/
+        {
+            animator.SetBool("isFalling", false);
+            //animator.SetBool("IsJumping", false);
 
+
+        }
     }
 
-    void Flip()
+       void Flip()
 
-    {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    
 }
